007192f31d972899e69cb7f36fe324125e149a23
rishabh-ioanyt <rishabh.maurya@ioanyt.com>
2023-02-09 13:41:38 +0530

    fully working spring first

--- src/main/java/com/example/springfirst/OAuth2AuthorizationServiceImpl.java
+++ src/main/java/com/example/springfirst/OAuth2AuthorizationServiceImpl.java
@@ -1,23 +1,36 @@
 package com.example.springfirst;
 
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
-import org.springframework.security.oauth2.server.authorization.context.AuthorizationServerContextHolder;
-import org.springframework.stereotype.Service;
+import java.util.Map;
 
-import java.util.Objects;
-
-@Service
 public class OAuth2AuthorizationServiceImpl implements OAuth2AuthorizationService {
+
+    private Map<String, OAuth2Authorization> initializedAuthorizations;
+    private final Map<String, OAuth2Authorization> authorizations;
+
+    public OAuth2AuthorizationServiceImpl(Map<String, OAuth2Authorization> initializedAuthorizations, Map<String, OAuth2Authorization> authorizations) {
+        this.initializedAuthorizations = initializedAuthorizations;
+        this.authorizations = authorizations;
+    }
 
     @Override
     public void save(OAuth2Authorization authorization) {
-        System.out.println(authorization.getId());
-        System.out.println(authorization.getPrincipalName());
-        System.out.println(authorization.getRegisteredClientId());
-        System.out.println(authorization.getAttributes());
+        if (isComplete(authorization)) {
+            System.out.println(authorization.getId());
+            System.out.println(authorization.getPrincipalName());
+            System.out.println(authorization.getRegisteredClientId());
+            System.out.println(authorization.getAccessToken().getToken().getTokenValue());
+            this.authorizations.put("first", authorization);
+        } else {
+            this.initializedAuthorizations.put(authorization.getId(), authorization);
+        }
+    }
 
+    private static boolean isComplete(OAuth2Authorization authorization) {
+        return authorization.getAccessToken() != null;
     }
 
     @Override
@@ -33,7 +46,12 @@ public class OAuth2AuthorizationServiceImpl implements OAuth2AuthorizationServic
 
     @Override
     public OAuth2Authorization findByToken(String token, OAuth2TokenType tokenType) {
-        System.out.println("find By Token");
+        System.out.println(token);
+        OAuth2Authorization.Token<OAuth2AccessToken> accessToken = authorizations.get("first").getAccessToken();
+            if(accessToken.getToken().getTokenValue().equals(token))
+                return authorizations.get("first");
         return null;
     }
+
+
 }
--- src/main/java/com/example/springfirst/userAccount/UserAccountService.java
+++ src/main/java/com/example/springfirst/userAccount/UserAccountService.java
@@ -1,19 +1,27 @@
 package com.example.springfirst.userAccount;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthenticationContext;
-import org.springframework.security.oauth2.server.authorization.context.AuthorizationServerContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 public class UserAccountService  {
 
 
     @GetMapping("/api/user")
-    public Object userDetails(){
+    public Map<String, Object> userDetails(){
+        Map<String, Object> objectMap = new HashMap<>();
+        objectMap.put("Name", Collections.singletonList("rishabh"));
+        objectMap.put("Granted Authorities",new ArrayList<>(Arrays.asList("ROLE_USER","SCOPE_read:user")));
+        Map<String, Object> map = new HashMap<>();
+        map.put("login","rishabh-ioanyt");
+        map.put("id","123456789");
+        map.put("type","User");
+        map.put("name","admin");
+        objectMap.put("User Attributes",map);
         System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        return AuthorizationServerContextHolder.getContext().getIssuer();
+        return objectMap;
     }
 }
